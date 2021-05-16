bf9e6affa4157988a55060087cfb58df99af8aac
minmyatthu <mmthu17mce@ytu.edu.mm>
2021-05-16 12:00:23 +0630

    finish finised page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import Start from "./pages/Start";
+import React from 'react'
+import Start from './pages/Start'
+import Finished from "./pages/Finished";
 
 function App() {
   return (
-    <div className='bg-gray-100 h-screen pt-8'>
-      <Start handleStart={() => null}/>
+    <div className="bg-gray-100 h-screen pt-8">
+      <Start handleStart={() => null} />
+      <Finished name={null} handleRestart={() => null} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/pages/Finished.tsx
+++ src/pages/Finished.tsx
@@ -1,14 +1,30 @@
 interface Props {
   name: string | null
 
+  handleRestart(): void
 }
 
-const Finished = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-};
+const Finished = (props: Props) => {
+  const { name, handleRestart } = props
 
-export default Finished;
+  return (
+    <div className="w-full max-w-xl mx-auto">
+      <h1 className='text-center uppercase tracking-wide font-bold py-3 mb-3.5 text-2xl text-blue-500' >
+        {name && `Player ${name} won the game`}
+        {!name && "It's a tie"}
+      </h1>
+      <div className="text-center">
+        <button
+          className="w-28 mt-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4
+            rounded focus:outline-none focus:shadow-outline transform hover:-translate-y-0.5
+            transition uppercase tracking-wider shadow-md"
+          onClick={handleRestart}
+        >
+          Restart
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Finished
